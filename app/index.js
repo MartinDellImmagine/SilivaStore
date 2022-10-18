41ab23441084f422d75abb491fbc4a1b6dcc4e3b
MartinDellImmagine <103152780+MartinDellImmagine@users.noreply.github.com>
2022-10-18 16:12:04 -0300

    fixxxxx

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@
 import Ofertas from "./funciones/Ofertas.js"
 import requestGet from "./helpers/requestGet.js"
 import carritoCompras from "./funciones/carritoCompras.js"
-import detalle from "./funciones/detalle.js"
+import detalle from "./funciones/detalleRedireccion.js"
 import pintarCarro from "./funciones/PintarCarro.js"
 
 // VARIABLES GLOBALES
@@ -15,6 +15,7 @@ const sectionOfertas = document.querySelector('#div-ofertas')
 
 //Programa Principal
 requestGet('../productos.json').then(datos=>{
+    //console.log(datos);
     sectionOfertas.appendChild(Ofertas(datos)),
     carritoCompras(datos),
     detalle(datos),
